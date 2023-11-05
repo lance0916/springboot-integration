98ad07ed10db761c4b14b44456310d3c23356194
WuQinglong <wuqinglong@didiglobal.com>
2023-11-05 10:47:18 +0800

    commit

---
+++ web-template/src/main/java/com/example/DemoCheckStyle.java
@@ -0,0 +1,19 @@
+package com.example;
+
+/**
+ * @author WuQinglong
+ * @since 2023/11/5 8:25 AM
+ */
+public class DemoCheckStyle {
+
+
+
+    /**
+     * @deprecated 推荐使用 xxx 替代
+     */
+    @Deprecated
+    public void f1() {
+
+    }
+
+}
--- web-template/src/main/java/com/example/controller/knife4j/Knife4jController.java
+++ web-template/src/main/java/com/example/controller/knife4j/Knife4jController.java
@@ -8,6 +8,7 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
